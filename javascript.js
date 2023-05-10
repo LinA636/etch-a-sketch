13566d010c27b989b4223faff4d4b0a988574f18
Lina <117147821+LinA636@users.noreply.github.com>
2023-05-10 21:23:07 +1200

    finalise all buttons, allocate them the proper color and opacity

--- javascript.js
+++ javascript.js
@@ -42,64 +42,61 @@ function handleInputNumber() {
     addListenerToGridFields();
 }
 
-function getColorClass(){
-    return document.querySelector(".button-clicked").getAttribute("data-key");
-}
-
-function clearSketchPad(){
+function clearSketchPad() {
+    const gridContainer = document.querySelector(".grid-container");
+    const bgColor = getComputedStyle(gridContainer).backgroundColor;
     const gridFieldNodeList = document.querySelectorAll(".grid-field");
     gridFieldNodeList.forEach(gridField => {
-        manipulateColorClasses(gridField);
+        gridField.style.backgroundColor = bgColor;
+        gridField.style.opacity = 1;
     });
 }
 
-function manipulateColorClasses(gridField, selectedColorClass){
-    const colorButtonNodeList = document.querySelectorAll(".color-button");
-    colorButtonNodeList.forEach((colorButton) => {
-        const colorClass = colorButton.getAttribute("data-key");
-        if(colorClass === selectedColorClass){
-            gridField.classList.add(selectedColorClass);
-        } else {
-            gridField.classList.remove(colorClass);
-        }
-    });
+function generateRandNumBetw0And255() {
+    return Math.floor(Math.random() * 256);
 }
 
-function generateRandNumBetw0And255(){
-    return Math.floor(Math.random()*256);
-}
-
-function generateRandRGBColor(){
+function generateRandRGBColor() {
     const r = generateRandNumBetw0And255();
     const g = generateRandNumBetw0And255();
     const b = generateRandNumBetw0And255();
-    const randRGB = `rgb(${r},${g},${b})`; 
+    return `rgb(${r},${g},${b})`;
 }
 
-function addBackgroundColor(gridField, selectedColorClass){
-    console.log(selectedColorClass);
-    if(selectedColorClass === "black-color"){
+function makeShadow(gridField) {
+    const styleOfGridField = getComputedStyle(gridField);
+    let opacityOfGridField = Number(styleOfGridField.opacity);
+    if (opacityOfGridField === 1) {
+        gridField.style.opacity = 0.1;
+    } else {
+        gridField.style.opacity = opacityOfGridField + 0.1;
+    }
+}
+
+function addBackgroundColor(gridField, selectedColorClass) {
+    if (selectedColorClass === "black-color") {
         gridField.style.backgroundColor = "black";
-    }else if(selectedColorClass === "shadow-color"){
-        const styleOfGridField = getComputedStyle(gridField);
-        let opacityOfGridField = styleOfGridField.opacity;
-        console.log(opacityOfGridField);
-        opacityOfGridField = 0.1;
-        console.log(opacityOfGridField);
-        gridField.style.opacity = opacityOfGridField;
-        gridField.style.backgroundColor = "grey";
-    }else if(selectedColorClass === "rainbow-color"){
-        gridField.style.backgroundColor = `${generateRandRGBColor()}`;
-    }else if(selectedColorClass === "eraser-color"){
+        gridField.style.opacity = 1;
+    } else if (selectedColorClass === "shadow-color") {
+        makeShadow(gridField);
+        gridField.style.backgroundColor = "black";
+    } else if (selectedColorClass === "rainbow-color") {
+        gridField.style.backgroundColor = generateRandRGBColor();
+        gridField.style.opacity = 1;
+    } else if (selectedColorClass === "eraser-color") {
         const gridContainer = document.querySelector(".grid-container");
         const bgColor = getComputedStyle(gridContainer).backgroundColor;
         gridField.style.backgroundColor = bgColor;
+        gridField.style.opacity = 1;
     }
+}
+
+function getColorClass() {
+    return document.querySelector(".button-clicked").getAttribute("data-key");
 }
 
 function paintSketchPad(gridField) {
     const selectedColorClass = getColorClass();
-    /* manipulateColorClasses(gridField, selectedColorClass); */
     addBackgroundColor(gridField, selectedColorClass);
 }
 
